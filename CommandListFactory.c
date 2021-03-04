48add8cf54b7d3fa3b44a73da8f0010bc48a0bc3
sim-mcqueen <s.mcqueen@digipen.edu>
2021-03-04 13:53:03 -0800

    Update CommandListFactory.c

--- CommandListFactory.c
+++ CommandListFactory.c
@@ -33,34 +33,42 @@ CommandList* CreateCommandList()
 				additional command handler functions by looking here: CommandHandlerFunctions.h */
 	CommandList_Add(cmdListPtr, "help", HandleHelpCommand, true);
 	/* TODO BASIC: add support for a short alias "h" */
+	CommandList_Add(cmdListPtr, "h", HandleHelpCommand, true);
 
 	/* TODO REQUIRED: create the "quit" command
 	TODO BASIC: add a short alias "q" */
 	CommandList_Add(cmdListPtr, "quit", HandleQuitCommand, true);
+	CommandList_Add(cmdListPtr, "q", HandleQuitCommand, true);
 
 	/* TODO REQUIRED: create the "drop" command
 	TODO BASIC: add a short alias "d" */
 	CommandList_Add(cmdListPtr, "drop", HandleDropCommand, true);
+	CommandList_Add(cmdListPtr, "d", HandleDropCommand, true);
 
 	/* TODO REQUIRED: create the "take" command
 	TODO BASIC: add a short alias "t" */
 	CommandList_Add(cmdListPtr, "take", HandleTakeCommand, true);
+	CommandList_Add(cmdListPtr, "t", HandleTakeCommand, true);
 
 	/* TODO REQUIRED: create the "use" command
 	TODO BASIC: add a short alias "u" */
 	CommandList_Add(cmdListPtr, "use", HandleUseCommand, true);
+	CommandList_Add(cmdListPtr, "u", HandleUseCommand, true);
 
 	/* TODO REQUIRED: create the "look" command
 	TODO BASIC: add a short alias "l" */
 	CommandList_Add(cmdListPtr, "look", HandleLookCommand, true);
+	CommandList_Add(cmdListPtr, "l", HandleLookCommand, true);
 
 	/* TODO REQUIRED: create the "inventory" command
 	TODO BASIC: add a short alias "i" */
 	CommandList_Add(cmdListPtr, "inventory", HandleInventoryCommand, true);
+	CommandList_Add(cmdListPtr, "i", HandleInventoryCommand, true);
 
 	/* TODO REQUIRED: create the "go" command
 	TODO BASIC: add a short alias "g" */
 	CommandList_Add(cmdListPtr, "go", HandleGoCommand, true);
+	CommandList_Add(cmdListPtr, "g", HandleGoCommand, true);
 
 	/* TODO ADVANCED: create aditional commands
 	this should require the creation of additional .c files to implement the command functions */
