4f8422fb038f0000a1ffbdd0b15491907df90ad2
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2021-03-09 15:59:34 -0800

    Add rooms to world

--- WorldDataFactory.c
+++ WorldDataFactory.c
@@ -188,14 +188,20 @@ WorldData* CreateInitialWorldData()
 
 	/* TODO REQUIRED: update room count to match the number of rooms you have created and added to the world
 	   if this number doesn't match then your game will either crash or you will end up stuck in a broken room with no exits */
-	int roomCount = 1;
+	int roomCount = 8;
 
 	/* create the new WorldData object with 3 rooms */
-	worldData = WorldData_Create("Welcome to my GAM100 Game!\n\n", roomCount);
+	worldData = WorldData_Create("GAM100!\n\n", roomCount);
 
 	/* build each room and assign them to the world data */
 	WorldData_SetRoom(worldData, 0, Room0_Build());
-	/* TODO REQUIRED: add rooms 1 and 2 to the world data */
+	WorldData_SetRoom(worldData, 1, Room1_Build());
+	WorldData_SetRoom(worldData, 2, Room2_Build());
+	WorldData_SetRoom(worldData, 3, Room3_Build());
+	WorldData_SetRoom(worldData, 4, Room4_Build());
+	WorldData_SetRoom(worldData, 5, Room5_Build());
+	WorldData_SetRoom(worldData, 6, Room6_Build());
+	WorldData_SetRoom(worldData, 7, Room7_Build());
 
 	/* TODO ADVANCED: add additional advanced rooms */
 
